98e32e5f3a8ae05df7e577403acf69df97c1c587
kTrzcinskii <kt.trzcinski@gmail.com>
2024-07-15 22:08:54 +0200

    Handle `BinaryExpression` in `Interpreter`

--- sharplox/Services/Interpreter.cs
+++ sharplox/Services/Interpreter.cs
@@ -12,7 +12,39 @@ public class Interpreter : IExpressionVisitor<object?>
     
     public object? VisitBinaryExpression(BinaryExpression binaryExpression)
     {
-        throw new NotImplementedException();
+        var left = Evaluate(binaryExpression.Left);
+        var right = Evaluate(binaryExpression.Right);
+
+        switch (binaryExpression.Operator.Type)
+        {
+            case TokenType.MINUS:
+                return (double)left - (double)right;
+            case TokenType.PLUS:
+                if (left is double ld && right is double rd)
+                    return ld + rd;
+                if (left is string ls && right is string rs)
+                    return ls + rs;
+                break;
+            case TokenType.SLASH:
+                return (double)left / (double)right;
+            case TokenType.STAR:
+                return (double)left * (double)right;
+            case TokenType.GREATER:
+                return (double)left > (double)right;
+            case TokenType.GREATER_EQUAL:
+                return (double)left >= (double)right;
+            case TokenType.LESS:
+                return (double)left < (double)right;
+            case TokenType.LESS_EQAUL:
+                return (double)left <= (double)right;
+            case TokenType.EQUAL_EQUAL:
+                return AreEqual(left, right);
+            case TokenType.BANG_EQAUl:
+                return !AreEqual(left, right);
+        }
+
+        // This code should never be reached
+        return null;
     }
 
     public object? VisitGroupingExpression(GroupingExpression groupingExpression)
@@ -45,12 +77,21 @@ public class Interpreter : IExpressionVisitor<object?>
     // Bool -> just value
     // Nil -> always false
     // Everything else -> always true
-    public bool IsTruthy(object? value)
+    private bool IsTruthy(object? value)
     {
         if (value == null)
             return false;
         if (value is bool b)
             return b;
         return true;
+    }
+
+    private bool AreEqual(object? lhs, object? rhs)
+    {
+        if (lhs == null && rhs == null)
+            return true;
+        if (lhs == null || rhs == null)
+            return false;
+        return lhs.Equals(rhs);
     }
 }
